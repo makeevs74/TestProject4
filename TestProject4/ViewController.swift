3c8ecf5b3511fae18c0827cfdda9430b6f7dc2e4
makeevs74 <makeevs74@gmail.com>
2022-08-09 11:37:05 +0500

    Initial Commit

---
+++ TestProject4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject4
+//
+//  Created by Sergey Makeev on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
